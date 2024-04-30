179edc809b3821b53c01daa8b9cdc2a3698a930a
Nahuel Gomez <nngomez@fi.uba.ar>
2024-04-30 17:47:50 -0300

    add new db

---
+++ config/db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_URL = "postgresql://dbname_86dq_user:v6iqY7V5Dku4ZzC8ROVejPtWUEZC0GTT@dpg-cool5le3e1ms73b7tj80-a.oregon-postgres.render.com/dbname_86dq"
+
+engine = create_engine(DATABASE_URL)
+
+meta = MetaData()
+conn = engine.connect()
