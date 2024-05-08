6c85f39d97159e43c2bc67db968d6db3ea48e9f1
NahuelNGomez <nngomez@fi.uba.ar>
2024-05-08 17:49:40 -0300

    fix cors

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ app.include_router(group)
 
 origins = [
     "http://localhost:5173",
+
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
